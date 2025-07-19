724eee9ea6f8b9484a852aa67687fe96f4626539
Nga <thungaho2004@gmail.com>
2025-07-19 09:09:54 +0700

    chore: add initial frontend folde

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "../layouts/MainLayout/MainLayout"
+import SearchPage from "../features/paperSearch/pages/searchPage/searchPage"
+
+function App() {
+  return (
+    <MainLayout>
+      <SearchPage />
+    </MainLayout>
+  )
+}
+
+export default App
